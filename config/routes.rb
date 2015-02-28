3754371f8855602312b9a2cdb15a8b5dc5630215
joshspears <josh@woofound.com>
2015-02-28 16:10:55 -0500

    Add login conditions to devise and other links

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
- 
+   
+  root 'pages#home'
 
+  get 'about' => 'pages#about'
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
 
-  get 'about' => 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
